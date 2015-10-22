f5775033ab555c5dc0604b3163d9d2f3cff8dd07
Gavin Meyers <gameyers@cisco.com>
2015-10-22 12:22:57 +0530

    commented out precedence annotation

--- src/main/java/com/github/gavinmeyers/ebookmanager/EbookmanagerApplication.java
+++ src/main/java/com/github/gavinmeyers/ebookmanager/EbookmanagerApplication.java
@@ -2,11 +2,20 @@ package com.github.gavinmeyers.ebookmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class EbookmanagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EbookmanagerApplication.class, args);
+    }
+    
+    @Bean
+    //@Order(Ordered.HIGHEST_PRECEDENCE)
+    public StartupRunner schedulerRunner() {
+      return new StartupRunner();
     }
 }
