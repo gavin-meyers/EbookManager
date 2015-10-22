e6e768da7a6fe68499b4b511c038b4ac6559f47e
Gavin Meyers <gameyers@cisco.com>
2015-10-22 14:15:51 +0530

    created a REST resource out of a repository object

--- src/main/java/com/github/gavinmeyers/ebookmanager/store/EBookStore.java
+++ src/main/java/com/github/gavinmeyers/ebookmanager/store/EBookStore.java
@@ -4,20 +4,22 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.github.gavinmeyers.ebookmanager.model.EBook;
 
-
+@RepositoryRestResource(collectionResourceRel = "ebooks", path = "myEbooks")
 public interface EBookStore extends MongoRepository<EBook, String> {
 
 	@Query("{ 'title' : ?0 }")
-	public EBook findByTitle(String title);
+	public EBook findByTitle(@Param("title") String title);
 
 	@Query("{ 'type' : ?0 }")
-	public List<EBook> findByType(String type);
+	public List<EBook> findByType(@Param("type") String type);
 
 	@Query("{ 'author' : ?0 }")
-	public List<EBook> findByAuthor(String author);
+	public List<EBook> findByAuthor(@Param("author") String author);
 
 	@Query("{ 'type' : ?0 }")
 	public List<EBook> findByBookType(String type);
