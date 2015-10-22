1388a4c7383b8fc5f4595dcd73e7179bfdafcfaf
Gavin Meyers <gameyers@cisco.com>
2015-10-22 08:37:19 +0530

    Initial commit Spring Boot , gradle, and other dependencies and bootsrap code

---
+++ src/main/java/com/github/gavinmeyers/ebookmanager/EbookmanagerApplication.java
@@ -0,0 +1,12 @@
+package com.github.gavinmeyers.ebookmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookmanagerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/github/gavinmeyers/ebookmanager/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.gavinmeyers.ebookmanager;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EbookmanagerApplication.class);
+	}
+
+}
