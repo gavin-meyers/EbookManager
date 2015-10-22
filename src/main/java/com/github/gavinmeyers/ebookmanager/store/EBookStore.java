cdabb0adb371beb366ffc66aea8736f898125c7a
Gavin Meyers <gameyers@cisco.com>
2015-10-22 14:08:50 +0530

    added Query annotation to support MongoDB level queries

--- src/main/java/com/github/gavinmeyers/ebookmanager/store/EBookStore.java
+++ src/main/java/com/github/gavinmeyers/ebookmanager/store/EBookStore.java
@@ -3,15 +3,26 @@ package com.github.gavinmeyers.ebookmanager.store;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.github.gavinmeyers.ebookmanager.model.EBook;
 
+
 public interface EBookStore extends MongoRepository<EBook, String> {
 
+	@Query("{ 'title' : ?0 }")
 	public EBook findByTitle(String title);
 
+	@Query("{ 'type' : ?0 }")
 	public List<EBook> findByType(String type);
 
+	@Query("{ 'author' : ?0 }")
 	public List<EBook> findByAuthor(String author);
+
+	@Query("{ 'type' : ?0 }")
+	public List<EBook> findByBookType(String type);
+
+	@Query("{ 'type' : {$ne : ?0} }")
+	public List<EBook> findByBookTypeNot(String type);
 
 }
